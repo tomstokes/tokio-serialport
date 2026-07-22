956ae87ac34ec0222754a5d51548dced3857a28d
Tom Stokes <tomstokes@radixengineering.com>
2026-07-22 14:41:04 -0600

    macos: Basic open port functionality
    Implement the basic functionality to open a port and get an AsyncFd for it.
    
    Does not yet configure the port.
    
    In testing this takes 2.7ms with a fast system and well-behaved drivers. I left a TODO to convert this to async.
    
    Signed-off-by: Tom Stokes <tomstokes@radixengineering.com>

--- examples/println.rs
+++ examples/println.rs
@@ -2,6 +2,9 @@ use tokio_serialport::SerialPort;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let _port = SerialPort::open("/dev/ttyUSB0", 115200).await?;
+    let path = std::env::args_os()
+        .nth(1)
+        .expect("Please provide a path to a serial port");
+    let _port = SerialPort::open(path, 115200).await?;
     Ok(())
 }
--- src/sys/macos.rs
+++ src/sys/macos.rs
@@ -1,13 +1,32 @@
 use crate::Settings;
+use std::os::fd::OwnedFd;
+use std::os::unix::fs::OpenOptionsExt;
 use std::path::Path;
+use tokio::io::unix::AsyncFd;
 
-pub(crate) struct Port {}
+pub(crate) struct Port {
+    fd: AsyncFd<OwnedFd>,
+}
 
 impl Port {
-    pub(crate) async fn open(
-        _path: impl AsRef<Path>,
-        _settings: Settings,
-    ) -> std::io::Result<Self> {
-        Ok(Self {})
+    pub(crate) async fn open(path: impl AsRef<Path>, _settings: Settings) -> std::io::Result<Self> {
+        // This function takes 2.7ms on my fast Mac with good drivers. The opening really should
+        // be done async because this could take a lot longer on slower machines
+        // TODO: Make opening async
+        let file = std::fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(false)
+            .custom_flags(libc::O_NONBLOCK | libc::O_NOCTTY | libc::O_CLOEXEC)
+            .open(path)?;
+        let fd = OwnedFd::from(file);
+
+        // TODO: Set exclusivity
+
+        // TODO: Configure according to settings
+
+        let async_fd = AsyncFd::new(fd)?;
+
+        Ok(Self { fd: async_fd })
     }
 }

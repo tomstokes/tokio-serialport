3bf331db7d844fa0fcce017140db11b3767a4205
Tom Stokes <tomstokes@radixengineering.com>
2026-07-22 14:53:55 -0600

    macos: Open fd async
    Use tokio's spawn_blocking to open the FD on a separate thread.
    
    This avoids blocking the executor for a couple milliseconds or more during the open operation.
    
    The error and panic handling is a little messy but not too bad.
    
    Signed-off-by: Tom Stokes <tomstokes@radixengineering.com>

--- src/sys/macos.rs
+++ src/sys/macos.rs
@@ -9,24 +9,40 @@ pub(crate) struct Port {
 }
 
 impl Port {
-    pub(crate) async fn open(path: impl AsRef<Path>, _settings: Settings) -> std::io::Result<Self> {
-        // This function takes 2.7ms on my fast Mac with good drivers. The opening really should
-        // be done async because this could take a lot longer on slower machines
-        // TODO: Make opening async
-        let file = std::fs::OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(false)
-            .custom_flags(libc::O_NONBLOCK | libc::O_NOCTTY | libc::O_CLOEXEC)
-            .open(path)?;
-        let fd = OwnedFd::from(file);
-
-        // TODO: Set exclusivity
-
-        // TODO: Configure according to settings
-
+    pub(crate) async fn open(path: impl AsRef<Path>, settings: Settings) -> std::io::Result<Self> {
+        let path = path.as_ref().to_owned();
+        let fd = match tokio::task::spawn_blocking(move || open_port(&path, settings)).await {
+            Ok(result) => result?,
+            // `tokio::task::spawn_blocking` catches panics. Re-panic if that happens.
+            Err(error) if error.is_panic() => {
+                std::panic::resume_unwind(error.into_panic());
+            }
+            Err(error) => {
+                return Err(std::io::Error::new(std::io::ErrorKind::Interrupted, error));
+            }
+        };
         let async_fd = AsyncFd::new(fd)?;
-
         Ok(Self { fd: async_fd })
     }
+}
+
+/// Synchronously open and configure a serial port at the given path
+///
+/// Opening and configuring a serial port takes longer than 1ms in testing and therefore should not
+/// be allowed ot block the tokio executor. This function is intended to be used with
+/// `tokio::task::spawn_blocking` to run on a separate thread.
+fn open_port(path: &Path, settings: Settings) -> std::io::Result<OwnedFd> {
+    let file = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(false)
+        .custom_flags(libc::O_NONBLOCK | libc::O_NOCTTY | libc::O_CLOEXEC)
+        .open(path)?;
+    let fd = OwnedFd::from(file);
+
+    // TODO: Set exclusivity
+
+    // TODO: Configure according to settings
+
+    Ok(fd)
 }
